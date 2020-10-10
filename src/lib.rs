e32c304dc7c2094833d8999eff351f605ae2edef
Oliver Archer <oliver@archer.fi>
2020-10-10 19:06:30 +0100

    Fixed display trait impl

--- src/lib.rs
+++ src/lib.rs
@@ -93,26 +93,25 @@ pub fn matrix(input: TokenStream) -> TokenStream {
     let x_dim = input.dimensions.0;
     let y_dim = input.dimensions.1;
 
-    TokenStream::from(quote! {
-        {
-            use std::fmt::{Display, Formatter, Result};
-            #[derive(Debug)]
-            pub struct Matrix([i32; #output_len]);
+    TokenStream::from(quote! {{
+        use std::fmt::{Display, Formatter, Result};
+        #[derive(Debug)]
+        pub struct Matrix([i32; #output_len]);
 
-            impl Display for Matrix {
-                fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-                    for i in 0..#x_dim {
-                        if i == 0 { write!(f, "[")? } else { write!(f, " ")? };
-                        for j in 0..#y_dim {
-                            write!(f, "{:>3},", self.0[#y_dim*i+j])?;
-                        }
-                        if i == #x_dim-1 { write!(f, "]")? } else { write!(f, "\n")? };
+        // Prints the user inputted array in the same dimensions as the passed in
+        impl Display for Matrix {
+            fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+                for i in 0..#x_dim {
+                    if i == 0 { write!(f, "[")? } else { write!(f, " ")? };
+                    for j in 0..#y_dim {
+                        write!(f, "{:>3},", self.0[#y_dim*i+j])?;
                     }
-                    Ok(())
+                    if i == #x_dim-1 { write!(f, "]")? } else { write!(f, "\n")? };
                 }
+                Ok(())
             }
-
-            Matrix(#output_arr)
         }
-    })
+
+        Matrix(#output_arr)
+    }})
 }
